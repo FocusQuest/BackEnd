782e9a597197dc944a285ec58cc60c3c5d8903c3
Maria Gabriela M Oliveira <magabi1610@gmail.com>
2023-11-04 18:22:31 -0300

    adicionado tabela Prioridade ao BD

---
+++ backend_BD_Rotas/prisma/migrations/20231104194925_adicionando_prioridade/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `idPrioridade` on the `Chamado` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Chamado" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nomeChamado" TEXT NOT NULL,
+    "descChamado" TEXT NOT NULL,
+    "dataAberturaChamado" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "idUsuario" INTEGER NOT NULL,
+    "idSuporte" INTEGER,
+    "idLab" INTEGER NOT NULL,
+    "idComputador" INTEGER NOT NULL,
+    "idCategoria" INTEGER NOT NULL,
+    "idAndamento" INTEGER NOT NULL DEFAULT 1,
+    "tratInicio" DATETIME,
+    "tratFim" DATETIME,
+    CONSTRAINT "Chamado_idUsuario_fkey" FOREIGN KEY ("idUsuario") REFERENCES "Usuario" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Chamado_idSuporte_fkey" FOREIGN KEY ("idSuporte") REFERENCES "Usuario" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Chamado_idLab_fkey" FOREIGN KEY ("idLab") REFERENCES "Lab" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Chamado_idComputador_fkey" FOREIGN KEY ("idComputador") REFERENCES "Computador" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Chamado_idCategoria_fkey" FOREIGN KEY ("idCategoria") REFERENCES "Categoria" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Chamado_idAndamento_fkey" FOREIGN KEY ("idAndamento") REFERENCES "Andamento" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Chamado" ("dataAberturaChamado", "descChamado", "id", "idAndamento", "idCategoria", "idComputador", "idLab", "idSuporte", "idUsuario", "nomeChamado", "tratFim", "tratInicio") SELECT "dataAberturaChamado", "descChamado", "id", "idAndamento", "idCategoria", "idComputador", "idLab", "idSuporte", "idUsuario", "nomeChamado", "tratFim", "tratInicio" FROM "Chamado";
+DROP TABLE "Chamado";
+ALTER TABLE "new_Chamado" RENAME TO "Chamado";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
